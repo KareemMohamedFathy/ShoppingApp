4a4cdae719614ddf8fe967f64207ab316c2f19cc
kareem <kareemfathy312@gmail.com>
2021-10-14 21:19:52 +0200

    added editing products

--- app/src/main/java/com/shopping/shoppingapp/SellerHomePage/MyProducts/EditProduct.kt
+++ app/src/main/java/com/shopping/shoppingapp/SellerHomePage/MyProducts/EditProduct.kt
@@ -139,6 +139,7 @@ fun EditProduct(navController: NavController,index:String,myProductsViewModel: M
                         arrayList.add(tag)
                     }
 
+
                     val pp=Product(name.text,price.text,description.text,productsList[idx].images,productsList[idx].shop_id,productsList[idx].product_id,arrayList,productsList[idx].time)
                     myProductsViewModel.editProduct(idx,pp)
                     navController.navigate(Screen.MyProducts.route){
